731e01759fb35ba1aa76a76536451703f7cd8d68
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-07-01 13:32:12 +0000

    .

--- 1t/src/app/page.tsx
+++ 1t/src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import {UserButton} from '@clerk/nextjs'
+import {SignIn, SignInButton, UserButton} from '@clerk/nextjs'
 
 export default function Home() {
 	return (
@@ -7,6 +7,11 @@ export default function Home() {
 			<h1 className="text-8xl font-extrabold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent text-center animate-text">
 				Welcome Page
 			</h1>
+			<div>
+				<button className="btn btn-wide">
+					<SignInButton mode="modal" />
+				</button>
+			</div>
 			<div>
 				<UserButton afterSignOutUrl="/" />
 			</div>
