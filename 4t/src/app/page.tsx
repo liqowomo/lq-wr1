de2cb2392aadcb5e2e1cb420af8111566787d57f
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-07-02 01:05:31 +0000

    .

--- 4t/src/app/page.tsx
+++ 4t/src/app/page.tsx
@@ -3,15 +3,17 @@ import Image from 'next/image'
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<section
-				id="HadPanty"
-				className="text-8xl font-extrabold text-transparent bg-gradient-to-r from-blue-600 via-yellow-400 to-rose-500 bg-clip-text animate-text text-center animate-rotate-x animate-infinite animate-duration-[20000ms]"
-			>
-				Liq
-				<br />
-				her
-				<br />
-				Now
+			<section>
+				<h1
+					id="HadPanty"
+					className="text-6xl font-extrabold text-transparent bg-gradient-to-r from-blue-600 via-yellow-400 to-rose-500 bg-clip-text text-center animate-text"
+				>
+					Liq
+					<br />
+					her
+					<br />
+					Now
+				</h1>
 			</section>
 		</main>
 	)
