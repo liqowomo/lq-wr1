513ec7f34d9f3d48f17b16f228ca06e5e67399aa
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-07-02 18:55:41 +0000

    .

--- 8t/src/app/page.tsx
+++ 8t/src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import Sigb from './components/sigb'
-import {SignInButton} from '@clerk/nextjs'
+import {SignInButton, UserButton} from '@clerk/nextjs'
 import {auth} from '@clerk/nextjs'
 
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<section>
+				<UserButton afterSignOutUrl="/" />
 				<h1
 					id="HadPanty"
 					className="text-6xl font-extrabold text-transparent bg-gradient-to-r from-blue-600 via-yellow-400 to-rose-500 bg-clip-text text-center animate-text"
