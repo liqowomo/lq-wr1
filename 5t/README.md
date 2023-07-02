d1742949c513fbca6e1254afef389eb99764db9a
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-07-02 13:21:04 +0000

    .

--- 5t/README.md
+++
@@ -1,8 +0,0 @@
-# Description
-
-[` Authentication and user management in 5 minutes - https://youtu.be/xazOE62k6AY`](https://youtu.be/xazOE62k6AY)
-
-- Using this one has shizinga 
-
-
-
--- 7t/src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import {authMiddleware} from '@clerk/nextjs'
-
-export default authMiddleware()
-
-//TODO : Config for routes
-
-export const config = {
-	matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-}
