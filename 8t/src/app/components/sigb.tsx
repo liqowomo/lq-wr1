88fe8169ce65602ad9ad50c71710002998931edc
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-07-02 18:48:26 +0000

    .

--- 8t/src/app/components/sigb.tsx
+++ 8t/src/app/components/sigb.tsx
@@ -4,10 +4,6 @@ import {SignInButton} from '@clerk/nextjs'
 
 export default function Sigb() {
 	return (
-		<section data-theme="synthwave">
-			<SignInButton mode="modal">
-				<button className="btn glass">Glass button</button>
-			</SignInButton>
-		</section>
+		<div>lll</div>
 	)
 }
