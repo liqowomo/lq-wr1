ae57e0a136cd29cf913e5ecee1e04b13350da91e
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-07-02 18:44:30 +0000

    .

--- 8t/src/app/(auth)/sign-in/page.tsx
+++ 8t/src/app/(auth)/sign-in/page.tsx
@@ -2,8 +2,27 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return ( 
-    <section className="flex min-h-screen flex-col items-center justify-center p-4">
+    <section>
+      <section id="bganimation">
+					<video
+						muted
+						autoPlay
+						loop
+						playsInline
+						disablePictureInPicture
+						className="absolute h-full w-full object-cover -z-10 blur-lg brightness-50"
+					>
+						<source
+							src="https://media.giphy.com/media/DKO203YtkprL6HkUfk/giphy.mp4"
+							type="video/mp4"
+						/>
+					</video>
+				</section>
+      <div className="flex min-h-screen flex-col items-center justify-center p-4">
       <SignIn />
+      </div>
+      <div>
+      </div>
     </section>
   )
 }
--- 8t/src/app/page.tsx
+++ 8t/src/app/page.tsx
@@ -1,12 +1,24 @@
-import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
-
+import Sigb from './components/sigb'
+import {SignInButton} from '@clerk/nextjs'
+import {auth} from '@clerk/nextjs'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-       <UserButton afterSignOutUrl="/"/>
-      <h1 className="text-6xl">Hello World</h1>
-    </main>
-  )
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<section>
+				<h1
+					id="HadPanty"
+					className="text-6xl font-extrabold text-transparent bg-gradient-to-r from-blue-600 via-yellow-400 to-rose-500 bg-clip-text text-center animate-text"
+				>
+					Liq
+					<br />
+					her
+					<br />
+					Now
+				</h1>
+				<Sigb />
+			</section>
+		</main>
+	)
 }
