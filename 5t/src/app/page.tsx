3324ad3dd2a165d4e25510b80125cd39d951d4b9
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-07-02 07:10:35 +0000

    .

--- 5t/src/app/page.tsx
+++ 5t/src/app/page.tsx
@@ -3,8 +3,11 @@ import Cr from './components/cr'
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<Cr />
+		<main className="flex min-h-screen flex-col items-center justify-around p-24">
+			<div className="border">
+				<Cr />
+			</div>
+			<h1 className="text-4xl font-extrabold">YumYum </h1>
 		</main>
 	)
 }
