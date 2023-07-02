488229e5f460d40e33249be51e45081a2b9a366e
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-07-02 06:58:51 +0000

    .

---
+++ 5t/src/app/components/cr.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+export default function Cr() {
+	return (
+		<section>
+			<div className="carousel rounded-box">
+				<div className="carousel-item">
+					<img
+						src="https://media1.giphy.com/media/CZedlPSpZ6hKo/giphy.gif"
+						alt="Burger"
+					/>
+				</div>
+				<div className="carousel-item">
+					<img
+						src="https://media1.giphy.com/media/4ftxO9bk1OJgI/giphy.gif"
+						alt="Burger"
+					/>
+				</div>
+				<div className="carousel-item">
+					<img
+						src="https://media1.giphy.com/media/WeiYU1ea1Poqs/giphy.gif"
+						alt="Burger"
+					/>
+				</div>
+				<div className="carousel-item">
+					<img
+						src="https://media1.giphy.com/media/eMO2b37vlirjFEMxwh/giphy.gif"
+						alt="Burger"
+					/>
+				</div>
+				<div className="carousel-item">
+					<img
+						src="https://media1.giphy.com/media/aA2FXW9zgaIYU/giphy.gif"
+						alt="Burger"
+					/>
+				</div>
+				<div className="carousel-item">
+					<img
+						src="https://media0.giphy.com/media/9CCraoy22z5ja/giphy.gif"
+						alt="Burger"
+					/>
+				</div>
+				<div className="carousel-item">
+					<img
+						src="https://media4.giphy.com/media/TZ6WMHCpM2mfS/giphy.gif"
+						alt="Burger"
+					/>
+				</div>
+			</div>
+		</section>
+	)
+}
