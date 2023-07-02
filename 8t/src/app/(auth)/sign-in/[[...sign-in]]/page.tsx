5c3c79567789e3a1306ceec96c37daa72a972495
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-07-02 19:44:41 +0000

    .

--- 8t/src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ 8t/src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,24 +1,5 @@
 import {SignIn} from '@clerk/nextjs'
 
 export default function Page() {
-	return (
-		<section>
-			<section id="bganimation">
-				<video
-					muted
-					autoPlay
-					loop
-					playsInline
-					disablePictureInPicture
-					className="absolute h-full w-full object-cover -z-10 blur-lg brightness-50"
-				>
-					<source
-						src="https://media.giphy.com/media/DKO203YtkprL6HkUfk/giphy.mp4"
-						type="video/mp4"
-					/>
-				</video>
-				<SignIn />
-			</section>
-		</section>
-	)
+	return <SignIn />
 }
