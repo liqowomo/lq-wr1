d677c73f7db40d03aecaa2f1fcc305a8c324486a
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-07-02 01:18:54 +0000

    .

--- 4t/src/app/(protected)/b1/page.tsx
+++ 4t/src/app/(protected)/b1/page.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import Txa from '@/app/components/txa'
+import {Dancing_Script} from 'next/font/google'
+
+const dan = Dancing_Script({subsets: ['latin']})
 
 export default function page() {
-  return (
-    <div>page</div>
-  )
+	return (
+		<section className={dan.className}>
+			<section id="bganimation">
+				<video
+					muted
+					autoPlay
+					loop
+					playsInline
+					disablePictureInPicture
+					className="absolute h-full w-full object-cover -z-10 blur-lg brightness-50"
+				>
+					<source
+						src="https://i.giphy.com/media/mX1bWOEGPIAY1yhmKI/giphy.mp4"
+						type="video/mp4"
+					/>
+				</video>
+			</section>
+			<section className="flex min-h-screen flex-col items-center justify-around p-24">
+				<h1 className="text-6xl text-center">Logged in Dawg</h1>
+				<div>
+					<Txa />
+				</div>
+			</section>
+		</section>
+	)
 }
---
+++ 4t/src/app/components/txa.tsx
@@ -0,0 +1,26 @@
+export default function Txa() {
+	return (
+		<section data-theme="luxury" className="flex flex-col items-center gap-5">
+			<textarea
+				placeholder="Bio"
+				className="textarea textarea-bordered textarea-lg w-full max-w-2xl"
+			></textarea>
+			<ul className="menu bg-base-200 w-56 rounded-box">
+				<li>
+					<a>Item 1</a>
+				</li>
+				<li>
+					<a>Item 2</a>
+				</li>
+				<li>
+					<a>Item 3</a>
+				</li>
+			</ul>
+			<div className="mockup-window border bg-base-300">
+				<div className="flex justify-center px-4 py-16 bg-base-200">
+					<code>import bigBreasted ladies from fetishqueen</code>
+				</div>
+			</div>
+		</section>
+	)
+}
