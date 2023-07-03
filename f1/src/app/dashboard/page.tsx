76c1e4130e331fc810bbbec1d893c0c0bc30fa91
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-07-03 18:07:22 +0000

    .

--- f1/src/app/dashboard/page.tsx
+++ f1/src/app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+import {UserButton} from '@clerk/nextjs'
+
 export default function page() {
 	return (
 		<section>
@@ -16,6 +18,9 @@ export default function page() {
 					/>
 				</video>
 			</section>
+			<div>
+				<UserButton />
+			</div>
 		</section>
 	)
 }
