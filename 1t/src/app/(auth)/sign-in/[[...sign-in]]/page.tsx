7f20fb94dab72d44b099f412f8f08498c773ce94
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-07-03 10:07:18 +0000

    .

--- 1t/src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ 1t/src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,5 @@
 import {SignIn} from '@clerk/nextjs'
 
 export default function Page() {
-	const SignInPage = () => (
-		<SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
-	)
+	return <SignIn />
 }
--- 1t/src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ 1t/src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,7 +1,5 @@
 import {SignUp} from '@clerk/nextjs'
 
 export default function Page() {
-	const SignUpPage = () => (
-		<SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
-	)
+	return <SignUp />
 }
