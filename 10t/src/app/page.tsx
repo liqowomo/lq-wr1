d68331403d04fea90d0dbb7d77f3fe4d1777f6cb
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-07-03 12:33:25 +0000

    .

--- 10t/src/app/page.tsx
+++ 10t/src/app/page.tsx
@@ -1,12 +1,15 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<h1 className='tex-8xl'>
-				Welcome
-			</h1>
-			
+		<main className="flex min-h-screen flex-col items-center justify-center p-24">
+			<Link href="/sign-in">
+				<button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">
+					Responsive
+				</button>
+			</Link>
+			<h1 className="text-8xl">Welcome</h1>
 		</main>
 	)
 }
