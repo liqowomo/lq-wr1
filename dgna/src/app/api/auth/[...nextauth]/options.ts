840ec95cfdf1467f9de0314e4bf2be06fabad9cc
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-07-04 12:50:40 +0000

    .

--- dgna/src/app/api/auth/[...nextauth]/options.ts
+++ dgna/src/app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,12 @@
 import type {NextAuthOptions} from 'next-auth'
+import GitHubProvider from 'next-auth/providers/github'
+import {CredentialsProvider} from 'next-auth/providers/credentials'
 
 export const options: NextAuthOptions = {
-	providers: [],
+	providers: [
+		GitHubProvider({
+			clientId: process.env.GITHUB_CLIENT_ID as string, 
+			clientSecret: process.env.GITHUB_CLIENT_SECRET as string
+		}),
+	],
 }
