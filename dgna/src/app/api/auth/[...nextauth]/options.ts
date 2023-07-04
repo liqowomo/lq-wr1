13d854f0d177ce76ef85ff76544025221cf9f2bd
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-07-04 12:59:26 +0000

    .

--- dgna/src/app/api/auth/[...nextauth]/options.ts
+++ dgna/src/app/api/auth/[...nextauth]/options.ts
@@ -1,12 +1,33 @@
 import type {NextAuthOptions} from 'next-auth'
 import GitHubProvider from 'next-auth/providers/github'
-import {CredentialsProvider} from 'next-auth/providers/credentials'
+import CredentialsProvider from 'next-auth/providers/credentials'
 
 export const options: NextAuthOptions = {
 	providers: [
 		GitHubProvider({
-			clientId: process.env.GITHUB_CLIENT_ID as string, 
-			clientSecret: process.env.GITHUB_CLIENT_SECRET as string
+			clientId: process.env.GITHUB_CLIENT_ID as string,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+		}),
+		CredentialsProvider({
+			name: 'Credentials',
+			credentials: {
+				username: {label: 'Username', type: 'text', placeholder: 'nameBastard'},
+				password: {label: 'Password', type: 'password'},
+			},
+			async authorize(credentials) {
+				// This is where you need to retrieve the user from your database
+				// to verify the credentials
+				const user = {id: '69', name: 'BootyAddict', password: 'z'}
+
+				if (
+					credentials?.username === user.name &&
+					credentials?.password === user.password
+				) {
+					return user
+				} else {
+					return null
+				}
+			},
 		}),
 	],
 }
