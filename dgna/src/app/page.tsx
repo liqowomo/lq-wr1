2b11fb9622844a79a368c19471a238659edf791d
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-07-04 15:00:29 +0000

    .

--- dgna/src/app/page.tsx
+++ dgna/src/app/page.tsx
@@ -1,17 +1,17 @@
-import {options} from './api/auth/[...nextauth]/options'
-import {getServerSession} from 'next-auth/next'
-import UserCard from './components/UserCard'
+import { options } from "./api/auth/[...nextauth]/options"
+import { getServerSession } from "next-auth/next"
+import UserCard from "./components/UserCard"
 
 export default async function Home() {
-	const session = await getServerSession(options)
+  const session = await getServerSession(options)
 
-	return (
-		<>
-			{session ? (
-				<UserCard user={session?.user} pagetype={'Home'} />
-			) : (
-				<h1 className="text-5xl">You Shall Not Pass!</h1>
-			)}
-		</>
-	)
+  return (
+    <>
+      {session ? (
+        <UserCard user={session?.user} pagetype={"Home"} />
+      ) : (
+        <h1 className="text-5xl">You Shall Not Pass!</h1>
+      )}
+    </>
+  )
 }
--- dgna/src/app/server/page.tsx
+++ dgna/src/app/server/page.tsx
@@ -1,19 +1,24 @@
-import { options } from "../../../../ref/dgNextAuthIntro/src/app/api/auth/[...nextauth]/options"
-import { getServerSession } from "next-auth/next"
-import UserCard from "../../../../ref/dgNextAuthIntro/src/app/components/UserCard"
-import { redirect } from "next/navigation"
+import {options} from '../../../../ref/dgNextAuthIntro/src/app/api/auth/[...nextauth]/options'
+import {getServerSession} from 'next-auth/next'
+import UserCard from '../../../../ref/dgNextAuthIntro/src/app/components/UserCard'
+import {redirect} from 'next/navigation'
+
+// =========================
+
+// Additions from codiums will be in this section 
+
+//==========================
 
 export default async function ServerPage() {
-    const session = await getServerSession(options)
+	const session = await getServerSession(options)
 
-    if (!session) {
-        redirect('/api/auth/signin?callbackUrl=/server')
-    }
+	if (!session) {
+		redirect('/api/auth/signin?callbackUrl=/server')
+	}
 
-    return (
-        <section className="flex flex-col gap-6">
-            <UserCard user={session?.user} pagetype={"Server"} />
-        </section>
-    )
-
+	return (
+		<section className="flex flex-col gap-6">
+			<UserCard user={session?.user} pagetype={'Server'} />
+		</section>
+	)
 }
